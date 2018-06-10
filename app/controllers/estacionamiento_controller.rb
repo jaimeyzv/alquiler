2da0ab5178e999cb6217c706b6d8ae34e1467efe
jaimeyzv <j_zamorav@hotmail.com>
2018-06-10 17:04:57 -0500

    busqueda cliente

--- app/controllers/estacionamiento_controller.rb
+++ app/controllers/estacionamiento_controller.rb
@@ -1,12 +1,21 @@
 class EstacionamientoController < PlantillaController
 
-  def busqueda
+  def busqueda_dueno
 	Rails.logger.debug("--------------> cargando busqueda" );
   end
   
-  def busqueda_post
+  def busqueda_dueno_post
     
     render "busqueda"
   end
+
+  def busqueda_cliente
+    Rails.logger.debug("--------------> cargando busqueda" );
+    end
+    
+    def busqueda_cliente_post
+      
+      render "busqueda"
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   get 'seguridad/login'
   post 'seguridad/login_post'
 
-  get 'estacionamiento/busqueda'
-  post 'estacionamiento/busqueda_post'
+  get 'estacionamiento/busqueda_dueno'
+  post 'estacionamiento/busqueda_dueno_post'
+
+  get 'estacionamiento/busqueda_cliente'
+  post 'estacionamiento/busqueda_cliente_post'
 
   get 'estacionamiento/mantenimiento'
   post 'estacionamiento/mantenimiento_post'
