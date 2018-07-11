26801ac7859a029970b886d29e334fb15169f8d8
CESAR HUACHUA <CESAR.HUACHUA@GMAIL.COM>
2018-07-11 01:28:18 -0500

    Manejo de sesion de usuario

--- app/controllers/seguridad_controller.rb
+++ app/controllers/seguridad_controller.rb
@@ -1,17 +1,46 @@
+require File.join(Rails.root, "app", "models", "Usuario.rb")
+
 class SeguridadController < ApplicationController
 
   def login
+  cookies.delete(:id_usuario)
+  cookies.delete(:nombre_usuario)
+  cookies.delete(:perfil_usuario)
 	Rails.logger.debug("--------------> cargando login" );
   end
   
   def login_post
-    @txt_nombre_producto = params[:txt_nombre_producto]   
-    @num_precio_producto = params[:num_precio_producto]
+    @usuario = params[:ccod_usu]   
+    @contrasena = params[:cpass_usu]
     
-    Rails.logger.debug("--------------> " + @txt_nombre_producto)
-    Rails.logger.debug("--------------> " + @num_precio_producto)
+    Rails.logger.debug("--------------> " + @usuario)
+    Rails.logger.debug("--------------> " + @contrasena)
+
+    @usuarios = Usuario.find_by_sql("SELECT * FROM usuario where correo ='"+@usuario+"' and contrasenia='"+@contrasena+"'")
     
-    render "login"
+    if @usuarios.nil?
+        Rails.logger.debug("--------------> esta vacio!... ")
+        @resultado = "Usuario o password inválidos"
+    else
+      usr = nil
+        @usuarios.each do |p|
+          usr = p
+          cookies[:id_usuario] = usr.IdUsuario.to_i
+          cookies[:nombre_usuario] = usr.Nombres.to_s
+          cookies[:perfil_usuario] = usr.TipoUsuario.to_s
+         # Rails.logger.debug("sesion--------------> " + usr.Nombres.to_s)
+        end
+       if usr == nil
+        @resultado = "Usuario o password inválidos"
+       end
+    end 
+
+    if usr == nil
+       render "login"
+    else
+       redirect_to '/estacionamiento/busqueda_cliente'
+    end
+
   end
 
 end
